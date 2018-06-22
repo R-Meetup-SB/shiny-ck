192ee3a14d109411d0f02484706e0db8821d8522
mikejohnson51 <mike.johnson@geog.ucsb.edu>
2018-06-21 19:09:58 -0700

    map code start

---
+++ map.R
@@ -0,0 +1,30 @@
+library(leaflet)
+
+pop <- paste(
+  paste("<Watershed:</strong>", data$Watershed),
+  paste("<strong>Creek:</strong>", data$`Creek or Stream`),
+  paste("<strong>Site Name:</strong>", data$`Site Name`),
+  paste("<strong>Site Name:</strong>", data$`Site Code`),
+  sep = "<br/>"
+)
+
+m = leaflet() %>%
+  addProviderTiles(providers$CartoDB.Positron, group = "Base") %>%
+  addProviderTiles(providers$Esri.WorldImagery, group = "Imagery") %>%
+  addProviderTiles(providers$Esri.NatGeoWorldMap , group = "Terrain") %>%
+  
+  addScaleBar("bottomleft") %>%
+  addMiniMap(tiles = providers$OpenStreetMap.BlackAndWhite,
+             toggleDisplay = TRUE) %>%
+  addMeasure(
+    position = "bottomleft",
+    primaryLengthUnit = "feet",
+    primaryAreaUnit = "sqmiles",
+    activeColor = "red",
+    completedColor = "green"
+  )  %>% addMarkers(data = locs, lng = locs$lon, lat = locs$lat, popup = pop ) %>% addLayersControl(
+    baseGroups = c("Base", "Imagery", "Terrain"),
+    options = layersControlOptions(collapsed = T)
+  )
+
+
